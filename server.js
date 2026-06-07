cc5ec67f86e32f13f4a3ad55689f772076c2d858
fourcade7 <FourcadeAminov7@gmail.com>
2026-06-07 20:23:39 +0500

    m

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
 import express from "express"
 import http from "http"
-//import { WebSocketServer } from "ws"
+import { WebSocketServer } from "ws"
 
 const app = express();
-// const server = http.createServer(app);
-// const wss=new WebSocketServer({server});
+const server = http.createServer(app);
+const wss=new WebSocketServer({server});
 
-// wss.on("connection", (ws,req)=>{
+wss.on("connection", (ws,req)=>{
 
-//     console.log("User Connected");
+    console.log("User Connected");
 
-//     const userId=req.headers.userid;
+    const userId=req.headers.userid;
+   
 
-//     ws.on("message",(msg)=>{
-//         console.log(msg.toString());
-//         ws.send("Negap new user");
-//         ws.send(userId);
-//     });
+    ws.on("message",(msg)=>{
+        console.log(msg.toString());
+        ws.send("Negap new user");
+        ws.send(userId);
+        
+    });
 
-//     ws.on("close",()=>{
-//         console.log("User disconnected");
-//     });
+    ws.on("close",()=>{
+        console.log("User disconnected");
+    });
 
-//     ws.on("error", (err) => {
-//         console.error("❌ Xatolik:", err);
-//     });
+    ws.on("error", (err) => {
+        console.error("❌ Xatolik:", err);
+    });
 
     
 
-// });
+});
 
 app.get("/",(req,res)=>{
     res.send("PR")
 })
-app.listen(3000,()=>{
+server.listen(3000,()=>{
     console.log("Server is running");
 })
